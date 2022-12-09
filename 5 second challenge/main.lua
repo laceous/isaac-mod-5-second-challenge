605f4ca8255437e9935819bf7a4e21028d2a8fd5
laceous <laceous@gmail.com>
2022-12-08 19:26:54 -0800

    update shift index

--- 5 second challenge/main.lua
+++ 5 second challenge/main.lua
@@ -8,6 +8,7 @@ mod.allowCountdown = false
 mod.incrementAttempt = false
 mod.roomTime = 5
 mod.rng = RNG()
+mod.rngShiftIndex = 35
 mod.font = Font()
 mod.fonts = {}
 mod.kcolor = KColor(255/255, 255/255, 255/255, 1) -- white w/ full alpha
@@ -474,7 +475,7 @@ function mod:seedRng()
   repeat
     local rand = Random()  -- 0 to 2^32
     if rand > 0 then       -- if this is 0, it causes a crash later on
-      mod.rng:SetSeed(rand, 1)
+      mod.rng:SetSeed(rand, mod.rngShiftIndex)
     end
   until(rand > 0)
 end
